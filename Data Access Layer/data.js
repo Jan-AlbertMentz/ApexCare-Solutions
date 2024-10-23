7d1288677c6768e729f620524d068378000d16c6
Jan-Albert Mentz <131801266+Jan-AlbertMentz@users.noreply.github.com>
2024-10-23 12:14:48 +0200

    Update data.js

--- Data Access Layer/data.js
+++ Data Access Layer/data.js
@@ -98,7 +98,7 @@ const sendMail = (email) => {
     from: 'senproject384@gmail.com',
     to: `${email}`,
     subject: 'Apexcare || Thank you!',
-    text: `Thank you for using our services.\n\nPlease use the following link to leave a review or raise a ticket:\nhttp://localhost:5000/review?email=${encodeURIComponent(email)}`
+    text: `Thank you for using our services.\n\nPlease use the following link to leave a review or raise a ticket:\nhttps://apexcaresolutionscrmapp1-caxriv03.b4a.run//review?email=${encodeURIComponent(email)}`
   };
 
   transporter.sendMail(mailOptions, function (error, info) {
